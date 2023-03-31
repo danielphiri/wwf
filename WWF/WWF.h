d29d8e5cc9afeb162f2e898b19571c36a1e1a5e4
danielphiri <danielphiri@berkeley.edu>
2023-03-31 15:56:06 -0700

    Initial Commit

---
+++ WWF/WWF.h
@@ -0,0 +1,18 @@
+//
+//  WWF.h
+//  WWF
+//
+//  Created by Daniel Phiri on 3/31/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WWF.
+FOUNDATION_EXPORT double WWFVersionNumber;
+
+//! Project version string for WWF.
+FOUNDATION_EXPORT const unsigned char WWFVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WWF/PublicHeader.h>
+
+
